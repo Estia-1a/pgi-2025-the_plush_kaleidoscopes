5b7fa9b697c4d5fe14131dd698ce43f38579e580
timorexe <189098939+timorexe@users.noreply.github.com>
2025-06-18 15:59:11 +0200

    scale_crop done

--- src/features.c
+++ src/features.c
@@ -4,13 +4,6 @@
 #include "features.h"
 #include "utils.h"
 
-/**
- * @brief Here, you have to code features of the project.
- * Do not forget to commit regurlarly your changes.
- * Your commit messages must contain "#n" with: n = number of the corresponding feature issue.
- * When the feature is totally implemented, your commit message must contain "close #n".
- */
-
 void helloWorld() {
     printf("Hello World !!!");
 }
